cd2fd8c88305f0cfc6a1437119c525f72557f5d6
jiwonkim425 <75045807+jiwonkim425@users.noreply.github.com>
2020-11-25 20:24:11 -0600

    Update database.py

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ import sqlite3
 """
 Using SQLite directly in Python to store top 5/10 user-score data.
 SQL lets us work with a relational database efficiently
+Using parameterized queries to prevent SQL injection 
  """
-
 # defining connector & cursor
 # Setting up the connection with simonSays database;
 # If database.db doesn't exist, it'll be created
@@ -15,37 +15,83 @@ csr = db.cursor()
 
 def createTable():
     # a method to create a table scores to keep track of top 5 high scores
-    csr.execute("CREATE TABLE IF NOT EXISTS scores(name TEXT, score INTEGER)")
+    table = "scores"
+    fields = "(name TEXT, score INTEGER)"
+
+    sql_query = (f"CREATE TABLE IF NOT EXISTS {table} {fields}")
+    csr.execute(sql_query)
+    db.commit()
 
 def newHighScore(newName, newScore):
-    # a method to update a new high score in scores table
-    csr.execute("INSERT INTO scores (name, score) VALUES (?, ?)", (newName, newScore))
+    """
+    a method to insert new high score in scores table
+    :param newName, newScore:
+    :return: None
+    """
+    table = "scores"
+    fields = "(name, score)"
+
+    sql_query = (f"INSERT INTO scores {fields} "
+                 f"VALUES (?, ?)")
+    csr.execute(sql_query, (newName, newScore))
+    db.commit()
 
 def showTopFive():
     """
-    a method that shows top five user-score in descending order
-    :return a list with 5 tuples - in (name, score) format:
+    a method that selects top five user-score in descending order, sorted by score
+    :param: None
+    :return: a list with 5 tuples - in (name, score) format:
     """
-    # Showing all the components in descending order here for now
-    topFive = list(csr.execute("SELECT name, score FROM scores ORDER BY score DESC LIMIT 5"))
-    return topFive
+    table = "scores"
+    fields = "name, score"
+    order = "score"
+    condition = "DESC LIMIT 5"
+
+    sql_query = (f"SELECT {fields} "
+                 f"FROM {table} "
+                 f"ORDER BY {order} "
+                 f"{condition}")
+
+    players = [x for x in csr.execute(sql_query)]
+    return players
 
 def close():
     db.commit() # a method to commit to the changes made in the database
     db.close()  # a method to close SQL connection
 
 # Optional method for now
-def deleteScore(name, score):
-    # a method to delete the row with the lowest score from scores table
-    # by looking up minimum score value & deleting that row
-    csr.execute("DELETE FROM scores WHERE score = (SELECT min(score) FROM scores)")
+def deleteLowestScore():
+    """
+    a method to delete the row with the lowest score from scores table
+    by looking up minimum score value
+    :param: None
+    :return: None
+    """
+    table = "scores"
+    condition = "(SELECT min(score) FROM scores)"
+    sql_query = (f"DELETE FROM {table} "
+                 f"WHERE score = {condition}")
+    csr.execute(sql_query)
+    db.commit()
 
-# Optional method for now
-def findPlayer(playerName):
+def findPlayer(name):
     """
     a method to check if playerName has been stored in database
-    :param playerName we are trying to find:
-    :return True if the name is already in the database
-            False if the name is not in the database:
+    :param name: playerName we are trying to find
+    :return: True if the name is already in the database
+            False if the name is not in the database
     """
-    csr.execute("SELECT name FROM scores", playerName)
+    table = "scores"
+    field = "name, score"
+    condition = "name = ?"
+
+    sql_query = (f"SELECT {field} "
+                 f"FROM {table} "
+                 f"WHERE {condition}")
+
+    players = [x for x in csr.execute(sql_query, (name,))]
+
+    if len(players) == 0:   # If a row with name is NOT found in the db
+        return False
+    else:                   # If a row with name is found
+        return True
